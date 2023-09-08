b61a164d966c82e7dd0c037fd3c74bfad6fbe3bd
varsil suvagiya <varsilsuvagiya@gmail.com>
2023-09-08 20:55:38 +0530

    8-9-23

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -15,6 +15,7 @@ function Home() {
         <Container className="home-content">
           <Row>
             <Col md={7} className="home-header">
+            
               <h1 style={{ paddingBottom: 15 }} className="heading">
                 Hi There!{" "}
                 <span className="wave" role="img" aria-labelledby="wave">
