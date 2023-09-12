6a2ace31abf5694daed8febb6a53047eb098c455
varsil suvagiya <varsilsuvagiya@gmail.com>
2023-09-12 21:07:18 +0530

    12-9-23

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -22,7 +22,7 @@ function Footer() {
         </Col>
         <Col md="4" className="footer-body">
           <ul className="footer-icons">
-          
+
             <li className="social-icons">
               <a
                 href="https://github.com/varsilsuvagiya"
@@ -31,6 +31,7 @@ function Footer() {
                 rel="noopener noreferrer"
               >
                 <AiFillGithub />
+                
               </a>
             </li>
             <li className="social-icons">
