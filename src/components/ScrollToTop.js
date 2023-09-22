09e3bc6bb2220ce8224b1bea73f06a21b8ef4add
varsil suvagiya <varsilsuvagiya@gmail.com>
2023-09-22 20:41:44 +0530

    22-9

--- src/components/ScrollToTop.js
+++ src/components/ScrollToTop.js
@@ -3,6 +3,7 @@ import { useLocation } from "react-router-dom";
 
 function ScrollToTop() {
   const { pathname } = useLocation();
+  
   useEffect(() => {
     window.scrollTo(0, 0);
   }, [pathname]);
