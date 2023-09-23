e0b21ec1b9146d8db7a5f64a763166da1b999024
varsil suvagiya <varsilsuvagiya@gmail.com>
2023-09-23 20:43:44 +0530

    23-9

--- src/components/About/Github.js
+++ src/components/About/Github.js
@@ -4,6 +4,7 @@ import { Row } from "react-bootstrap";
 
 function Github() {
   return (
+
     <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
       <h1 className="project-heading" style={{ paddingBottom: "20px" }}>
         Days I <strong className="purple">Code</strong>
@@ -13,6 +14,7 @@ function Github() {
         blockSize={15}
         blockMargin={5}
         color="#c084f5"
+        
         fontSize={16}
       />
     </Row>
